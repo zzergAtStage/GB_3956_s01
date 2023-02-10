349baac76cdf67e784c05f72602a0e57b64bb510
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-10 21:55:41 +0300

    seminars 001

---
+++ S01_example001/Program.cs
@@ -0,0 +1,33 @@
+﻿int count = 0;
+while (count < 4) {
+    Console.WriteLine("Введите число 1");
+    String sA =  Console.ReadLine();
+    Console.WriteLine("Введите число 2");
+    String sB =  Console.ReadLine();
+
+  int a = Convert.ToInt32(sA);
+  int b = Convert.ToInt32(sB);
+    if ((b*b) == a ) {
+        Console.WriteLine("Квадрат числа а (" + b + ") = " + a);
+        }
+        else
+        {
+            Console.WriteLine("Квадрат числа а (" + b + ") Не! " + a);
+        }
+  count++;  
+}
+
+
+
+
+
+Console.WriteLine("Hello, World!");
+//я пишу в чат) Дарья хорошо кодит. Предлагаю сделать с ней)
+
+// У нас кодировка проекта неверная) 
+// А конкатенация? Наоборот!
+// B - квадрат 
+// я же пришел на середине семинара.
+//Иван каким методом принимал значения? Кто-нибудь успел скачать текстовый файл из чата?
+//Как преобразовывать в инт? Cast должен быть какойто
+
---
+++ S02_example002/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Input day of the week:");
+String sA = Console.ReadLine();
+  int a = Convert.ToInt32(sA);
+switch (a)
+{
+   case 1: 
+   Console.WriteLine("Monday");
+   break;
+  case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  case 3:
+    Console.WriteLine("Wednesday");
+    break;
+  case 4:
+    Console.WriteLine("Thursday");
+    break;
+  case 5:
+    Console.WriteLine("Friday");
+    break;
+  case 6:
+    Console.WriteLine("Saturday");
+    break;
+  case 7:
+    Console.WriteLine("Sunday");
+    default:
+    Console.WriteLine("Inpit another day");
+}
