ed11f918609d0680744d4264d486f423501e20b6
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-16 01:33:05 +0300

    s002 task02

--- S02_example001/Program.cs
+++ S02_example001/Program.cs
@@ -1,18 +1,16 @@
-﻿//Write programm shows max digit in random generated numbers in [10-99] 
+﻿//Write programm shows max digit in random generated numbers in [10-999] 
 //1st get a nember bay Random
 //convert number to array
 //find max and print
 
-int number = Random.Shared.Next(10,100);
+int number = Random.Shared.Next(10,1000);
 
 Console.WriteLine("Number = " + number);
 int lastDigit = number % 10;
-Console.WriteLine("Mod = " + lastDigit);
-int firstDigit = number / 10;
-if (firstDigit > lastDigit)
-{
-    Console.WriteLine("max = " + firstDigit );
-} else
-{
-    Console.WriteLine("max = " + lastDigit );
-}
+//Console.WriteLine("Mod = " + lastDigit);
+int firstDigit = number / 100;
+int middleDigit = (number / 10) % 10;
+
+Console.WriteLine($"numbers are: 1:{firstDigit} 2:{middleDigit} 3:{lastDigit}");
+int res = firstDigit * 10 + lastDigit;
+Console.WriteLine($"res = {res}");
