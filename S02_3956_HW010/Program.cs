d68df530adae66fc17b4a4e3a1ffdbe279a19b92
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-16 01:57:27 +0300

    s002 task10

--- S02_3956_HW010/Program.cs
+++ S02_3956_HW010/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int number = Convert.ToInt32(Console.ReadLine());//Random.Shared.Next(10,1000);
+int middleDigit = (number / 10) % 10;
+Console.WriteLine($"{number} -> {middleDigit} ");
