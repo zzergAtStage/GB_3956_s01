abcef8d7535a5e077e6e843a81b26d07c18bcee8
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-18 17:15:00 +0300

    added task023

---
+++ S03_3596_HWadd/task023/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Inpit number:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{num} ->");
+for (int i = 1; i <= num; ++i)
+{
+   Console.Write($"{Math.Pow(i,3)},");
+}
