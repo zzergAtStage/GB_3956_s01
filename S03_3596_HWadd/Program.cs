a1262e804f2924bc8e59c2994b553a88d99d4732
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-18 14:53:51 +0300

    S03 homework added additional task

---
+++ S03_3596_HWadd/Program.cs
@@ -0,0 +1,26 @@
+﻿// Доп. задание
+// Написать метод для разворота массива, не используя память под дополнительный массив
+
+class InvertArray
+{
+    static void InvertTheArray()
+    {
+        int[] arr = new int[] { 2, 5, 8, 0, 1, 7, 9 };
+        int currItem, lastItem, size;
+        size = arr.Length;
+        Console.WriteLine("Array before inverting: " + String.Join(",", arr));
+        for (int i = 0; i < size / 2; i++)
+        {
+            currItem = arr[i];
+            lastItem = arr[size - 1 - i];
+            arr[size - 1 - i] = currItem;
+            arr[i] = lastItem;
+        }
+        Console.WriteLine("Array AFTER inverting: " + String.Join(",", arr));
+    }
+
+    public static void Main(String[] args)
+    {
+        InvertTheArray();
+    }
+}
---
+++ S03_example001/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.Clear();
+Console.WriteLine("Module getSector => input 1; getVector input => 2");
+int choise = Convert.ToInt32(Console.ReadLine());
+if (choise == 1)
+{
+   getSector(); 
+} else if (choise == 2)
+{
+   double res = getVector();
+   Console.WriteLine("Length of vector is = " + res);
+}
+
+
+
+static void getSector()
+{
+    Console.Write("Input x:");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Input y:");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    if (x == 0 || y == 0)
+    {
+        Console.WriteLine("Wrong ınput!");
+        return;
+    }
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("The dot in first sector");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("The dot in fourth sector");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("The dot in second sector");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("The dot in third sector");
+    }
+    else
+    {
+        Console.WriteLine("Can not define the sector!");
+    }
+}
+
+
+// Length of vector is AB = sqr((xB - xA)^2 + (yB - yA)^2)
+static double getVector(){
+    
+    Console.Write("Input xA:");
+    double xA = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input yA:");
+    double yA = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input xB:");
+    double xB = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input yB:");
+    double yB = Convert.ToInt32(Console.ReadLine());
+
+
+    return (Math.Sqrt(Math.Pow((xB - xA),2) + Math.Pow((yB - yA),2))); 
+}
