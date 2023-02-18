3e872b5f2a42d4a93ea40d529ef1975ad70c0410
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-02-18 16:30:25 +0300

    beautify

--- S03_3956_HW19/Program.cs
+++ S03_3956_HW19/Program.cs
@@ -12,31 +12,37 @@
 class Program
 {
     public static bool ifDebug = false;
-    static bool checkPolindrom(int param1){
-        int param2, tmpValue,  doadd;
+    static bool checkPolindrom(int param1)
+    {
+        int param2, tmpValue, doadd;
         //Разбираем число на косточки, разворачиваем и сравниваем
         doadd = 10000;
         tmpValue = param1;
         param2 = 0;
-        while (tmpValue > 0){
+        while (tmpValue > 0)
+        {
             param2 += (tmpValue % 10) * doadd;
             tmpValue /= 10;
-            doadd /=10;
-            if (ifDebug) {Console.WriteLine($"Debug: tmpValue={tmpValue}; param2={param2}; doadd={doadd}; "); }       
+            doadd /= 10;
+            if (ifDebug) { Console.WriteLine($"Debug: tmpValue={tmpValue}; param2={param2}; doadd={doadd}; "); }
         }
         return (param1 == param2);
     }
-    
-    public static void Main(string[] args){
-       Console.Clear();
-       Program.ifDebug = false;
-       Console.Write("Input checked for polindromic value: ");
-       int param = Convert.ToInt32(Console.ReadLine());
-       if ((param % 10000) != 0)
-       if (checkPolindrom(param)){
-        Console.Write($"{param} => true");
-       } else {
-        Console.Write($"{param} => false");
-       }
+
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Program.ifDebug = false;
+        Console.Write("Input checked for polindromic value: ");
+        int param = Convert.ToInt32(Console.ReadLine());
+        if ((param % 10000) != 0)
+            if (checkPolindrom(param))
+            {
+                Console.Write($"{param} => true");
+            }
+            else
+            {
+                Console.Write($"{param} => false");
+            }
     }
 }
