defee7901550efd3d18e28ea463ed41deeff5b80
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-03-11 02:48:25 +0100

    065 added to final S09_Final_Project

--- S09_Final_Project/task063/Program.cs
+++ S09_Final_Project/task063/Program.cs
@@ -1,8 +1,31 @@
 ﻿namespace task063;
 class Program
 {
+    static string result = String.Empty;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int currentIteration = 1;
+        Console.Clear();
+        int N = Libraries.Library.GetNumber("Input N");
+        GetNaturalNumbers(N, currentIteration);
+        System.Console.WriteLine($"N = {N} -> \"{result}\"");
+    }
+    static void GetNaturalNumbers(int N, int currentIteration)
+    {
+        if (currentIteration > N)
+        {
+            return;
+        }
+        else
+        {
+            if (currentIteration == N) { //bantiki
+                result += currentIteration; 
+            } else { 
+                result += currentIteration + ", "; 
+                }
+            currentIteration++;
+            GetNaturalNumbers(N, currentIteration: currentIteration); //recursion
+        }
+
     }
 }
---
+++ S09_Final_Project/task065/Program.cs
@@ -0,0 +1,35 @@
+﻿using Libraries;
+namespace task065;
+class Program
+{
+    static string result = String.Empty;
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        int M = Library.GetNumber("Input M value");
+        int N = Library.GetNumber("Input N value");
+        if (M < N) 
+        {GetNaturalNumbersBetweenMN(M, N, M);
+        } else {
+            GetNaturalNumbersBetweenMN(N, M, N);
+        }
+        System.Console.WriteLine($"M = {M};N = {N} -> \"{result}\"");
+
+    }
+    static void GetNaturalNumbersBetweenMN(int M, int N, int step = 0)
+    {
+        
+        if (step > N) return;
+        result += step + ", ";
+        if (M > N)
+        { //revers
+            step--;
+        }
+        else
+        {
+            step++;
+        }
+        GetNaturalNumbersBetweenMN(M, N, step);
+    }
+
+}
